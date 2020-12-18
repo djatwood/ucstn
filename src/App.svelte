ea988a26d60ed6c546fc7a73542cbe315d3ad268
Daniel Atwood <daniel@atwood.io>
2020-12-17 21:04:36 -0500

    Initial commit

---
+++ src/App.svelte
@@ -0,0 +1,14 @@
+<script>
+	import Header from "./sections/header.svelte";
+</script>
+
+<style global>
+	@tailwind base;
+	@tailwind components;
+	@tailwind utilities;
+  </style>
+  
+
+<main>
+	<Header />
+</main>
