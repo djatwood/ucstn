65f7def7bae35a42a1991884a77037f90568ae25
Daniel Atwood <daniel@atwood.io>
2021-01-06 20:51:05 -0500

    List products alphabetically

--- src/main.js
+++ src/main.js
@@ -6,41 +6,41 @@ const app = new App({
     services: [
       {
         name: "Toilet Paper", color: "8675a9", img: "toilet-paper.png", services: [
-          "Toilet Paper",
-          "Standard Toilet Paper",
-          "Jumbo Roll Toilet Paper",
-          "Center Pull Toilet Paper",
-          "Pallet of Toilet Paper",
-          "Toilet Paper Holder",
-          "Toilet Paper Dispensers",
-          "Standard Roll Dispenser",
-          "Jumbo Roll Dispensers",
-          "Dual Roll Dispensers",
-          "Coreless Dispensers",
           "Center Pull Toilet Paper Dispenser",
+          "Center Pull Toilet Paper",
+          "Coreless Dispensers",
+          "Dual Roll Dispensers",
+          "Jumbo Roll Dispensers",
+          "Jumbo Roll Toilet Paper",
+          "Pallet of Toilet Paper",
+          "Standard Roll Dispenser",
+          "Standard Toilet Paper",
+          "Toilet Paper Dispensers",
+          "Toilet Paper Holder",
+          "Toilet Paper",
           "Toilet Seat Covers & Dispensers",
         ]
       },
       {
         name: "Paper Towels", color: "c3aed6", img: "paper-towels.png", services: [
-          "Paper Hand Towels & Dispensers)",
           "C-Fold Paper Towels",
-          "Multifold Paper Towels",
-          "Single Fold Towels",
           "Center Pull Paper Towels",
-          "Roll Paper Towels",
           "Kitchen Paper Towels",
-          "Paper Towel Dispensers",
+          "Multifold Paper Towels",
           "Pallet of Paper Towels",
+          "Paper Hand Towels & Dispensers)",
+          "Paper Towel Dispensers",
+          "Roll Paper Towels",
+          "Single Fold Towels",
         ]
       },
       {
         name: "Napkins", color: "efbbcf", img: "napkin.png", services: [
-          "Napkins & Dispensers",
           "Beverage Napkins",
           "Dinner Napkins",
-          "Luncheon Napkins",
           "Facial Tissues",
+          "Luncheon Napkins",
+          "Napkins & Dispensers",
         ]
       },
       {
