0587cbe1425a72ac6e6f1ecb9ab9bb9f31ef7d87
Daniel Atwood <daniel@atwood.io>
2021-01-14 21:35:50 -0500

    Updated credits

--- src/sections/footer.svelte
+++ src/sections/footer.svelte
@@ -1,9 +1,3 @@
-<style lang="postcss">
-  p {
-    @apply text-sm text-gray-300;
-  }
-</style>
-
 <footer class="bg-gray-900 px-4 py-2">
   <div class="content-width grid sm:grid-cols-2">
     <p class="mb-2 sm:mb-0">
@@ -11,9 +5,18 @@
       <a href="//atwood.io" target="_blank">Atwood.io</a>
     </p>
     <p class="sm:text-right">
+      <a class="mr-4" href="https://www.freepik.com/vectors/people">
+        Header by pch.vector
+      </a>
       <a href="https://www.iconfinder.com/emojious" target="_blank">
-        Icons made by iconPerk.com
+        Icons by iconPerk.com
       </a>
     </p>
   </div>
 </footer>
+
+<style lang="postcss">
+  p {
+    @apply text-sm text-gray-300;
+  }
+</style>
