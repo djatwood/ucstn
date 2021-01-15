bf3684c8b0a6df65288da2ff39051a99808683a9
Daniel Atwood <daniel@atwood.io>
2021-01-15 17:18:41 -0500

    netlify attribute didln't work

--- src/sections/contact.svelte
+++ src/sections/contact.svelte
@@ -3,10 +3,10 @@
     <h2>Request a quote</h2>
 
     <div class="grid gap-8 lg:grid-cols-2">
-      <form class="flex flex-col lg:row-span-2" netlify>
-        <label>Full Name <input type="text" name="name" /> </label>
-        <label>Email <input type="email" name="email" /> </label>
-        <label>Message <textarea name="message" cols="30" rows="10" /> </label>
+      <form class="flex flex-col lg:row-span-2" data-netlify="true">
+        <label>Full Name <input type="text" name="name" /></label>
+        <label>Email <input type="email" name="email" /></label>
+        <label>Message <textarea name="message" cols="30" rows="10" /></label>
         <input class="submit border-0" type="submit" value="Submit" />
       </form>
 
