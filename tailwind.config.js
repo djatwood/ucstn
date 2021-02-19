9d2f381efb586fd433632c6134b54f21e8aaa35e
Daniel Atwood <daniel@atwood.io>
2021-02-18 22:46:32 -0500

    Use tailwind purge instead of @fullhuman/postcss-purgecss

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,8 @@
 module.exports = {
-    purge: ['layouts/**/*.html'],
+    purge: {
+        enabled: true,
+        content: ['./layouts/**/*.html'],
+    },
     darkMode: false, // or 'media' or 'class'
     plugins: [],
 }
